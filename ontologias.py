463dd029e7cd32850248ab8b0bc6a55b5f15dbae
Carlos Moreno <carlosandresmorenovelez@gmail.com>
2016-03-12 09:35:30 -0500

    feat: Empresas y alojamientos finished.

---
+++ ontologias.py
@@ -0,0 +1,19 @@
+#Ontologias definidas para modelar el ambuto del ecoturismo y geografia
+from rdflib import Namespace
+
+#Turismo
+CRUZAR = Namespace('http://idi.fundacionctic.org/cruzar/turismo#')
+ACCO = Namespace('http://purl.org/acco/ns#') #Alojamientos
+FOOD = Namespace('http://purl.org/ontology/fo/Collection#')
+PLANT = Namespace('http://purl.obolibrary.org/obo/po.owl')
+ANIMALS = Namespace('http://purl.org/ontology/wo/')
+
+#Geografia
+GEONAMES = Namespace('http://www.geonames.org/ontology#')
+GEOPOS = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+GEOES = Namespace('http://geo.linkeddata.es/ontology/') #
+
+#Otros
+ORG = Namespace('http://www.w3.org/ns/org#')
+GR = Namespace('http://purl.org/goodrelations/v1#')#ontologia del e-comerce
+
